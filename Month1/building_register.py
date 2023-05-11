b184d5fe19aadef4e5bcb9724fda4e5c082f5001
io <stuart@kieser.co.za>
2023-05-11 13:13:07 +0200

    Building register update
    The building register has been update to include an account feature for payments and functions to fetch and display the account amount.

--- Month1/building_register.py
+++ Month1/building_register.py
@@ -6,9 +6,10 @@ tenants = []
 
 class Tenant:
     def __init__(self, fname, lname, num):
+        self.account = 0
         self.fname = str(fname)
         self.lname = str(lname)
-        self.num = int(num)
+        self.num = num
         self.room = random.choice(range(0, 500))
         if self.room in rooms:
             try:
@@ -22,10 +23,66 @@ class Tenant:
         print(f"Tenant: {fname, lname} has been added to the register")
         print(f"Room {self.room} is available")
 
+    def remove_tenant(self):
+        tenants.remove(self)
+        rooms.remove(self.room)
 
-Tenant("stuart", "kieser", "071")
+    def pay_tenant(self, amt):
+        self.account += amt
+
+    def check_account(self):
+        return self.account
 
 
+def pay_tenant_(fname, recepientname, amt):
+    # finding the tenant that is paying
+    for tenant in tenants:
+        if tenant.fname == fname:
+            for r in tenants:
+                if r.fname == recepientname:
+                    tenant.pay_tenant((-amt))
+                    r.pay_tenant(amt)
+
+
+# create_tenants(50)
+def show_tenants():
+    for tenant in tenants:
+        print(
+            f"Tenant Name: {tenant.fname}, Tenant Lname: {tenant.lname}, Tenant Number: {tenant.num}, Tenant room: {tenant.room}"
+        )
+
+
+def find_tenant(attr):
+    for tenant in tenants:
+        if (
+            tenant.fname == attr
+            or tenant.lname == attr
+            or tenant.num == attr
+            or tenant.room == attr
+        ):
+            print(
+                f"Tenant: {tenant.fname}, {tenant.lname}, number: {tenant.num}, room: {tenant.room} "
+            )
+
+
+def remove_tenant_(fname, lname):
+    for tenant in tenants:
+        if tenant.fname == fname and tenant.lname == lname:
+            tenant.remove_tenant()
+            print(f"Tenant: {fname, lname}, has been removed")
+            return
+
+
+def check_tenant_account(attr):
+    for tenant in tenants:
+        if tenant.fname == attr:
+            return f"{tenant.fname}: {tenant.account}"
+
+
+Tenant("stuart", "kieser", 171)
+Tenant("n", "f", 909)
+
+"""
 def create_tenants(number):
     fnames = ["harry", "george", "barry", "larry", "gary", "ben", "greg"]
     lnames = ["harary", "george", "barary", "larary", "garary", "been", "grog"]
@@ -36,10 +93,4 @@ def create_tenants(number):
         num = random.choice(range(500, 1000))
         Tenant(fname, lname, num)
     return tenants
-
-
-create_tenants(50)
-for tenant in tenants:
-    print(
-        f"Tenant Name: {tenant.fname}, Tenant Lname: {tenant.lname}, Tenant Number: {tenant.num}, Tenant room: {tenant.room}"
-    )
+"""

eed18757a48a9e62a1091360317091875a48717e
io <stuart@kieser.co.za>
2023-05-15 08:00:55 +0200

    Python web dev journey update
    learnt about template in heritance in bootstrap, and how you can inherit certain elements to make them common through out web pages and what not.

--- Month2/register/website/__init__.py
+++ Month2/register/website/__init__.py
@@ -3,10 +3,15 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name, r=3)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("new.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

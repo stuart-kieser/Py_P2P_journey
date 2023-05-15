078e0f17a86646b6b49f2a9c6623d4be1e35aae1
io <stuart@kieser.co.za>
2023-05-15 09:01:42 +0200

    Python journey update
    Did stuff on post and get

--- Month2/register/website/__init__.py
+++ Month2/register/website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,20 @@ def home():
 @app.route("/test")
 def test():
     return render_template("new.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":

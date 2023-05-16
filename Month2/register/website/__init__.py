05993140c0366a13ea7e7da096f1b8e473b7908c
io <stuart@kieser.co.za>
2023-05-16 20:23:58 +0200

    Database connection and storage for website
    Recently we covered database and storage for the website. We are yet to connect the two and run them however.

--- Month2/register/website/__init__.py
+++ Month2/register/website/__init__.py
@@ -1,9 +1,27 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = "hello"
-app.permanent_session_lifetime = timedelta(days=5)
+app.config["SQLALCHEMY_DATABASE-URI"] = "sqlite://users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+db = SQLAlchemy(app)
+
+
+# no were moving onto the logic behind it all
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column(db.String(20))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route("/")
@@ -30,10 +48,19 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+
         return render_template("user.html", user=user)
     else:
         flash(f"You have not logged in.")
@@ -46,8 +73,10 @@ def logout():
         user = session["user"]
         flash(f"You have been logged out {user}", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)

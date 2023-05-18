48b8a6c594d5b62e76b46ed21e501ec7d5001b1f
io <stuart@kieser.co.za>
2023-05-18 09:25:15 +0200

    More changes
    I figures out how to get the boot strap styling working as well so now it is just a case of figuring out the inheritance, getting stuff from the database and posting stuff to the database.

--- Month2/Project Website/app.py
+++ Month2/Project Website/app.py
@@ -8,7 +8,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+@app.route("/public")
+def public():
+    # access database and print the entire thing in order
+    return render_template("public.html")
+
+
+@app.route("/database")
+def database():
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":

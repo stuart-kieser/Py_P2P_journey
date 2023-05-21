e9c1e3f9014872dc01a0856fedb88b48a347bf21
io <stuart@kieser.co.za>
2023-05-21 12:21:12 +0200

    Updates
    more updates

--- Month2/Project Website/app.py
+++ Month2/Project Website/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, url_for, request, render_template, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
+# this variable, db, is the db for this entire app
+# db = SQLAlchemy()
 db = []
+
+
 # create an app instance
 app = Flask(__name__)
+
+db_main = "register.db"
+
+# contains the databse connection string
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + db_main
 
 
 @app.route("/")
@@ -28,19 +39,19 @@ def user_profile(user):
 @app.route("/database", methods=["GET"])
 def database_entries():
     if request.method == "GET":
-        db_data = request.form[str(db)]
-        return render_template("database_entries", db_data=db_data)
+        db = request.args.get("str")
+        return render_template("_db.html", db=db)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 """
-just a little update, right now, i am able to creat my own db 
+just a little update, right now, i am able to create my own db 
 with the list function in python to store data. 
 
 That data is also partially callable to the database url page
-I think i have figer out how the GET method works.
-the error says is cannot understand the request but it does 
+I think i have figure out how the GET method works.
+the error says it cannot understand the request but it does 
 list it so it is being received. 
 """

676aae7e9e192ae08f59217ac9f5131841ac008d
io <stuart@kieser.co.za>
2023-07-03 16:30:52 +0200

    Update blockchain.py
    Building register BC

--- Month 4 BlockChain and P2P data structures/BC/blockchain.py
+++ Month 4 BlockChain and P2P data structures/BC/blockchain.py
@@ -1,10 +1,14 @@
 from hashlib import sha256
+from typing import Optional
 import time
+
+GENESIS_HASH = "0" * 64
 
 
 def update_hash(*args):
     hashing_text = ""
     h = sha256()
+
     for arg in args:
         hashing_text += str(arg)
 
@@ -13,22 +17,47 @@ def update_hash(*args):
 
 
 class Block:
-    data = None
-    hash = None
-    nonce = 0
-    previous_hash = "0" * 64
+    # actual block structure
+    number: int
+    previous: Optional["Block"]
+    data: Optional[dict]
+    nonce: int
+    timestamp: any
 
-    def __init__(self, data, number=0):
-        self.data = data
+    def __init__(
+        self, number=0, previous=None, data=None, nonce=0, timestamp=any
+    ) -> None:
         self.number = number
+        self.data = data
+        self.previous = previous
+        self.nonce = nonce
+        self.timestamp = timestamp
 
-    def hash(self):
-        return update_hash(self.previous_hash, self.number, self.data, self.nonce)
+    def get_previous_hash(self):
+        if self.previous is None:
+            return GENESIS_HASH
+        else:
+            return self.previous.get_hash()
+
+    def get_hash(self):
+        return update_hash(self.number, self.previous, self.data, self.nonce)
+
+    def get_timestamp(self):
+        struct_time = time.localtime()
+        timestamp = time.mktime(struct_time)
+        return timestamp
 
     def __str__(self):
         return str(
-            "Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n"
-            % (self.number, self.hash(), self.previous_hash, self.data, self.nonce)
+            "Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\nTimestamp: %s\n"
+            % (
+                self.number,
+                self.get_hash(),
+                self.previous,
+                self.data,
+                self.nonce,
+                self.get_timestamp(),
+            )
         )
 
 
@@ -43,31 +72,42 @@ class Blockchain:
 
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].hash()
+            block.previous = self.chain[-1].get_hash()
         except IndexError:
             pass
 
         while True:
-            if block.hash()[: self.difficulty] == "0" * self.difficulty:
+            if block.get_hash()[: self.difficulty] == "0" * self.difficulty:
                 self.add(block)
                 break
             else:
                 block.nonce += 1
 
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous
+            _current = self.chain[i - 1].get_hash()
+            if (
+                _previous != _current
+                or _current[: self.difficulty] != "0" * self.difficulty
+            ):
+                return False
+            return True
+
 
 def main():
     blockchain = Blockchain()
-    database = ["genesis"]
 
-    num = 0
-    for data in range(50):
-        num += 1
-        struct_time = time.localtime()
-        timestamp = time.mktime(struct_time)
-        blockchain.mine(Block(timestamp, num))
+    num = len(blockchain.chain)
+
+    num += 1
+
+    for num in range(25):
+        blockchain.mine(Block(num, None, (num * num)))
 
     for block in blockchain.chain:
         print(block)
+        print(blockchain.isValid())
 
 
 if __name__ == "__main__":

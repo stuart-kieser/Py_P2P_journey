d8e0b4d2afac1ac935a0137058cc517f71e2d366
io <stuart@kieser.co.za>
2023-07-09 12:21:28 +0200

    Rendevous|UDPserver
    Rendevous disconnects afters the second connection, propagations works partially, but only 2 peers can enter into the network.

--- Month 4 BlockChain and P2P data structures/BC/rendevous.py
+++ Month 4 BlockChain and P2P data structures/BC/rendevous.py
@@ -7,45 +7,44 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("localhost", known_port))
 print(f"binding port to: {known_port}")
 
-condition = threading.Condition()
+
 clients = []
+propogation_running = False
 
 
 def propogate_clients():
     while True:
-        with condition:
-            condition.wait()
+        for client in clients:
+            client_addr, client_port = client
+            for other_client in clients:
+                other_client_addr, other_client_port = other_client
 
-            for client in clients:
-                client_addr, client_port = client
-
-                for other_client in clients:
-                    other_client_addr, other_client_port = other_client
-
-                    if client != other_client:
-                        sock.sendto(b"ready", address)
+                if client != other_client:
+                    try:
                         sock.sendto(
                             "{} {} {}".format(
                                 other_client_addr, other_client_port, known_port
                             ).encode(),
                             (client_addr, client_port),
                         )
+                    except ConnectionResetError:
+                        print("Client closed the connection", client)
+                    break
 
 
-thread = threading.Thread(target=propogate_clients)
-thread.start()
-
+propogation = threading.Thread(target=propogate_clients)
+propogation.start()
 
 while True:
-    data, address = sock.recvfrom(1024)
-
-    print("connection from: {}".format(address))
-    clients.append(address)
-
-    sock.sendto(b"ready", address)
-
-    with condition:
+    try:
+        data, address = sock.recvfrom(1024)
+        print("connecting from {}".format(address))
+        print(address, data)
+        sock.sendto(b"AKK", address)
         clients.append(address)
-        condition.notify_all()
-        print("propogating peer details to other peers")
+        print("Client List:", len(clients))
+        print(f"starting propogation: {propogation}")
+    except:
+        if ConnectionResetError:
+            print("Error receiving data")
         break
--- Month 4 BlockChain and P2P data structures/BC/udpclientserver.py
+++ Month 4 BlockChain and P2P data structures/BC/udpclientserver.py
@@ -13,29 +13,48 @@ BC node bans
 BC node updates
 """
 rendevous_server = ("localhost", 55555)
-
-msg_ = []
 clients = []
 
 PORT = int(input("select a port in the range 8000 - 9000:"))
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(("localhost", PORT))
-server.sendto(b"0", rendevous_server)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("localhost", PORT))
+sock.sendto(b"55555", rendevous_server)
+print(f"Binding on port: {PORT}, sending bytes to: {rendevous_server}")
 
+
+def listen():
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind(("localhost", PORT))
+
+    while True:
+        data = server.recv(1024)
+        print("\rpeer: {}\n> ".format(data.decode("utf-8")), end="")
+
+
+listener = threading.Thread(target=listen, daemon=True)
 
 while True:
-    data = server.recv(1024).decode()
+    data = sock.recv(1024).decode()
+    print(f"receving data:\n")
+    print(data)
 
-    if data.strip() == "ready":
+    if data.strip() == "AKK":
         print("checked in with server, waiting")
         break
 
-data = server.recv(1024).decode()
+print("Waiting to receive other client addresses")
+
+data = sock.recv(1024).decode()
+sock.close()
+listener.start()
+
+print(f"peer details:{data}")
+
 ip, sport, dport = data.split(" ")
 sport = int(sport)
 dport = int(dport)
-clients.append({ip: any, sport: int, dport: int})
+clients.append(data)
 
 
 print("\nGot peer")
@@ -43,48 +62,15 @@ print("ip:{}".format(ip))
 print("sport:{}".format(sport))
 print("dport:{}\n".format(dport))
 print("Punching hole...")
+print("Data exchange is ready\n")
 
 
-server.sendto(b"0", (ip, dport))
-
-print("Data exchange is ready")
+print("Listening for BC updates:")
 
 
 def show_nodes():
     return clients
 
-
-def listen():
-    while True:
-        data = server.recv(1024)
-        msg_.append(data)
-        print("\rpeer: {}\n> ".format(data.decode("utf-8")), end="")
-
-
-listener = threading.Thread(target=listen, daemon=True)
-listener.start()
-
-
-def broadcast():
-    while True:
-        for data in list(msg.queue):
-            try:
-                decoded_message = msg.decode("utf-8")
-                if decoded_message.startswith("BCU:"):
-                    message_parts = decoded_message.split(":")[1:]
-                    if len(message_parts) == 5:
-                        number, data, previous, nonce, timestamp = message_parts
-                    blockchain.mine(
-                        blockchain.Block(number, data, previous, nonce, timestamp)
-                    )
-                    server.sendto("AKK".encode(), sport)
-                else:
-                    server.sendto(msg, sport)
-            except:
-                msg.remove(data)
-
-
-t2 = threading.Thread(target=broadcast)
 
 while True:
     msg = input("> ")
@@ -93,6 +79,7 @@ while True:
     elif msg == "show_nodes()":
         print(list(show_nodes()))
     else:
-        for client in clients:
-            ip, sport, dport = client
-            server.sendto(msg.encode(), (ip, sport))
+        for data in clients:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            client_socket.sendto(msg.encode(), (ip, sport))
+            client_socket.close()
